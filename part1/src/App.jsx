f1b1464c92e1b7803b7582f56334a1b85045e671
Octavio Duarte <octiduarte@hotmail.com>
2024-03-05 20:59:25 -0300

    useState

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,38 +2,19 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Total from "./components/Total";
 import Hello from "./components/Hello";
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  
+import { useState } from "react";
 
-  return (  
-    <div>
-     <Header course={course.name} />
-      <Content course={course.parts}/>
-      <Total course={course.parts}/>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  );
-};
+const App = () => {
+  const [ counter, setCounter ] = useState("gas")
+
+  setTimeout(
+    () => setCounter(counter + " puto"),
+    1000
+  )
+
+  return (
+    <div>{counter}</div>
+  )
+}
 
 export default App;
