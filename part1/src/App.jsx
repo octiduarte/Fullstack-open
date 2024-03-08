7ee9437dcad140547f4ed0669f5b726edf112acc
octiduarte <octiduarte@hotmail.com>
2024-03-08 12:08:50 -0300

    agregado de History

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,17 +3,33 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 import Hello from "./components/Hello";
 import { useState } from "react";
+import Display from "./components/Display";
+import Button from "./components/Button";
+import History from "./components/History";
 
 const App = () => {
-  const [ counter, setCounter ] = useState("gas")
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  setTimeout(
-    () => setCounter(counter + " puto"),
-    1000
-  )
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
   )
 }
 
