af81d01bde9246b868be7dd21afb125db1e22411
Octavio <octiduarte@hotmail.com>
2024-08-06 12:04:27 -0300

    part3

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
 app.use(express.json())
+app.use(cors())
+
+const generateId = () => {
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => n.id))
+  : 0 
+  return maxId + 1
+}
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 let notes = [
   {
@@ -22,8 +38,21 @@ let notes = [
 ]
 
 app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
+  const body = request.body
+  if(!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
 })
 
@@ -54,8 +83,14 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-const PORT = 3001
+app.use(unknownEndpoint)
+app.use(requestLogger)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
